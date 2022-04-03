dae844d3ce3b62387490b9fd0d175088060f3f40
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2022-04-03 08:30:52 +0400

    added content type

--- templates/BetaLixt.Templates.Web.Standard/BetaLixT.Templates.Web.Standard.Api/Middleware/ResponseCacheMiddleware.cs
+++ templates/BetaLixt.Templates.Web.Standard/BetaLixT.Templates.Web.Standard.Api/Middleware/ResponseCacheMiddleware.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Options;
 using System.IO.Pipelines;
+using System.Net.Mime;
+using System.Collections.Immutable;
 
 namespace BetaLixT.Templates.Web.Standard.Api.Middleware
 {
@@ -13,6 +15,14 @@ namespace BetaLixT.Templates.Web.Standard.Api.Middleware
 		private ResponseCacheFilterOptions _options;
 		private IDistributedCache _cache;
 		private RequestDelegate _next;
+
+		// TODO add more content types
+		private static readonly ImmutableDictionary<byte, string> AllowedContentType = new Dictionary<byte, string> {
+			{0, MediaTypeNames.Application.Json},
+			{1, $"{MediaTypeNames.Application.Json}; charset=utf-8"},
+			{2, MediaTypeNames.Application.Xml},
+			{3, MediaTypeNames.Application.Pdf}
+		}.ToImmutableDictionary();
 
 		public ResponseCacheMiddleware(
 			RequestDelegate next,
@@ -47,30 +57,40 @@ namespace BetaLixT.Templates.Web.Standard.Api.Middleware
 				ctx.Response.Body = new MemoryStream();
 	
 				await this._next(ctx);
+				var contentType = AllowedContentType
+					.Where(x => x.Value == ctx.Response.ContentType)
+					.FirstOrDefault();
 				var resStream = ctx.Response.Body;
 
-				var age = (UInt16)DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-				
-				// - Caching response 
-				var data = new byte[resStream.Position + 3];
-				var stream = new MemoryStream(data);
-				stream.WriteByte((byte)ctx.Response.StatusCode);
-				stream.Write(BitConverter.GetBytes(age), 0, 2);
-				resStream.Position = 0;
-				resStream.CopyTo(stream);
-				stream.Close();
-				this._cache.Set(key, data);
+
+				// - Caching response
+				if (contentType.Value != null)
+				{				
+					var age = (UInt16)DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+					
+					var data = new byte[resStream.Position + 4];
+					var stream = new MemoryStream(data);
+					stream.WriteByte((byte)ctx.Response.StatusCode);
+					stream.WriteByte(contentType.Key);
+					stream.Write(BitConverter.GetBytes(age), 0, 2);
+					resStream.Position = 0;
+					resStream.CopyTo(stream);
+					stream.Close();
+					this._cache.Set(key, data);
+				}
 
 				// - Moving back to original stream	
 				resStream.Position = 0;
 				await resStream.CopyToAsync(oriStream);
 				ctx.Response.Body = oriStream;
+				resStream.Close();
             }
 			else
             {
 				ctx.Response.StatusCode = resp[0];
-				ctx.Response.Headers.Add("cache-age", BitConverter.ToUInt16(resp, 1).ToString());
-				await ctx.Response.Body.WriteAsync(resp, 3, resp.Length - 3);
+				ctx.Response.ContentType = AllowedContentType[resp[1]];
+				ctx.Response.Headers.Add("cache-age", BitConverter.ToUInt16(resp, 2).ToString());
+				await ctx.Response.Body.WriteAsync(resp, 4, resp.Length - 4);
             }
         }
     }
